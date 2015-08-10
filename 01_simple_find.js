07970dae5c3a2d1d7006ab54873e3bfb310c9ecf
Sam Genduso <sam.genduso@gmail.com>
2015-08-10 16:44:17 -0600

    find one and log address

--- 01_simple_find.js
+++ 01_simple_find.js
@@ -1,4 +1,11 @@
-var db = require('monk')('localhost/association-objects')
-var people = db.get('people')
-var employers = db.get('employers')
-var addresses = db.get('addresses')
+var db = require('monk')('localhost/association-objects');
+var people = db.get('people');
+var employers = db.get('employers');
+var addresses = db.get('addresses');
+
+people.findOne({name: 'Pearl Cremin'}).then(function (person) {
+  return addresses.findOne({_id: person.addressId}).then(function (address) {
+    person.address = address;
+    console.log(person.address);
+  });
+});
